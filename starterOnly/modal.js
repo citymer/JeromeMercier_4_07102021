5f819ef1c68901687e48b29f1d0e13c9aa8b1776
citymer <cityhunter8484@gmail.com>
2021-10-14 21:40:28 +0200

    correction html ,validateur ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,17 +15,18 @@ const btnX = document.querySelector(".close");//selectionne le bouton (X) du for
 const form = document.querySelector("form");//selectionne le formulaire (balise <form>)
 
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 btnX.addEventListener("click", closeX);//détecte le click sur le bouton (X)
 form.addEventListener('submit', (e) => {e.preventDefault();})// annule la fonction par défaut du bouton d'envoi
 
 
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 function closeX(){
   modalbg.style.display = "none";            // fonction qui ferme le formulaire en cliquant sur bouton (X)
 }
