47a318bf9a115fbbe1215c687b06555cf2d3ab7f
citymer <cityhunter8484@gmail.com>
2021-11-01 10:31:16 +0100

    correction editnav

--- js/fonction.js
+++ js/fonction.js
@@ -1,3 +1,11 @@
+function editNav() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+}
 // function qui fait apparaitre le formulaire
 function launchModal() {
   modalbg.style.display = "block";
